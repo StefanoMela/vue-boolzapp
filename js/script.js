96a3ff6f79c66b8203654cbe3c64871673d67de0
StefanoMela <stefano.mela25@gmail.com>
2023-09-04 17:24:14 +0200

    feat MS III

--- js/script.js
+++ js/script.js
@@ -4,7 +4,39 @@ createApp({
   data() {
     return {
       contacts,
+      
       activeUser: 0,
+
+      newMessage: {
+        date: '04/09/2023 16:30:00',
+        message: '',
+        status: 'sent'
+      },
+
+      newReply: {
+        date: '04/09/2023 16:31:00',
+        message: 'Ok!',
+        status: 'received'
+      },
     }
+  },
+  methods: {
+
+    addMesssage() {
+
+      let toWrite = { ...this.newMessage };
+
+      if (this.newMessage.message.length > 1){
+        this.contacts[this.activeUser].messages.push(toWrite);
+        this.newMessage.message = "";
+
+        setTimeout(this.addReply, 1000)
+      };
+  },
+    addReply() {
+
+      this.contacts[this.activeUser].messages.push(this.newReply)
+
+    },
   }
 }).mount('#app')
